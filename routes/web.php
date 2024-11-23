c4ec572b060d0240e8aa9ccb917daefb5817e85f
Yazan-AbdAlazeez <yazanazeez128@gmail.com>
2024-11-23 16:44:09 +0300

    "replace resource Route"

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,14 @@
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
-Route::resource('posts', PostController::class);
+route::get('/', [PostController::class, "index"])->name("posts.index");
+route::get('/create', [PostController::class, "create"])->name("posts.create");
+route::post('/store', [PostController::class, "store"])->name("posts.store");
+route::get('/edit/{post}', [PostController::class, "edit"])->name("posts.edit");
+route::put('/update/{post}', [PostController::class, "update"])->name("posts.update");
+route::delete('/delete/{post}', [PostController::class, "destroy"])->name("posts.destroy");
+route::get('/show/{post}', [PostController::class, "show"])->name("posts.show");
+
+
+
+// Route::resource('posts', PostController::class);
