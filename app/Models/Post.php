45344a08ce82a166eba2cd5e0a674303634afee8
Yazan-AbdAlazeez <yazanazeez128@gmail.com>
2024-11-29 13:50:03 +0300

    add multiple images

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -15,12 +15,12 @@ class Post extends Model
     ];
     public function getImagesAttribute($value)
     {
-        return json_decode($value, true) ?? []; // Decode JSON to array or return empty array
+        return json_decode($value, true) ?? [];
     }
 
-    // Mutator to convert array to JSON string when setting the images attribute
+
     public function setImagesAttribute($value)
     {
-        $this->attributes['images'] = json_encode($value); // Encode array to JSON
+        $this->attributes['images'] = json_encode($value);
     }
 }
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -5,9 +5,9 @@
         <h1>{{ $post->title }}</h1>
         <div class="d-flex flex-wrap mb-3">
             @foreach ($post->images as $image)
-                <div class="me-2"> <!-- Add margin-end for spacing -->
+                <div class="me-2">
                     <img src="/images/posts/{{ $image }}" class="img-fluid rounded" alt="{{ $post->title }}"
-                        style="max-width: 30rem;"> <!-- Adjust max-width as needed -->
+                        style="max-width: 30rem;">
                 </div>
             @endforeach
         </div>
